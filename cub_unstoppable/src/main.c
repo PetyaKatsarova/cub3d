c8d07f7772aedd78037ccfe26ee9833ecde6846b
Petya Katsarova <pskpetya@gmail.com>
2025-08-05 09:02:10 +0200

    sth

--- cub_unstoppable/src/main.c
+++ cub_unstoppable/src/main.c
@@ -6,7 +6,7 @@
 /*   By: petya <petya@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 15:13:27 by pekatsar          #+#    #+#             */
-/*   Updated: 2025/08/04 18:07:11 by petya            ###   ########.fr       */
+/*   Updated: 2025/08/05 09:00:39 by petya            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int main(void)
     
     // event hooks
     mlx_loop_hook(data.mlx, render_frame, &data);
-    mlx_hook(data.win, 2, 1L<<0, key_press, &data); 
+    mlx_hook(data.win, 2, 1L<<0, key_press, &data); // 
     mlx_hook(data.win, 3, 1L<<1, key_release, &data); 
     mlx_hook(data.win, 17, 0, close_window, &data);
     
