2bb864e65fd409dbee7b30af0d34c56e98c38e62
Jan-Hendrik Stuhring <jstuhrin@f1r5s1.codam.nl>
2025-09-05 17:03:14 +0200

    1. fixed bug with mouse movement 2. added one valid .cub for testing

--- cub_imgs_unstoppable/src/render/mouse_handler.c
+++ cub_imgs_unstoppable/src/render/mouse_handler.c
@@ -28,7 +28,7 @@ int	mouse_handler(int curr_x, int curr_y, t_data *d)
 {
 	static int	last_x = 0;
 	int			delta_x;
-	
+
 	(void)curr_y;
 	if (d->focus == false)
 	{
@@ -36,21 +36,10 @@ int	mouse_handler(int curr_x, int curr_y, t_data *d)
 		return (0);
 	}
 	mlx_mouse_hide(d->mlx, d->win);
-	delta_x = curr_x - last_x;
-	if (delta_x < -1)
-	{
-		d->pl->angle -= ROTATION_FACTOR;
-		if (d->pl->angle < 0)
-			d->pl->angle += 2 * M_PI;
-	}
-	if (delta_x > 1)
-	{
-		d->pl->angle += ROTATION_FACTOR;
-		if (d->pl->angle > 2 * M_PI)
-			d->pl->angle -= 2 * M_PI;
-	}
-	d->pl->delta_x = cos(d->pl->angle) * 5;
-	d->pl->delta_y = sin(d->pl->angle) * 5;
+	delta_x = curr_x - 800;
+	d->pl->angle += (double)delta_x * 0.0003;
+	d->pl->delta_x = cos(d->pl->angle) * MOVE_SPEED;
+	d->pl->delta_y = sin(d->pl->angle) * MOVE_SPEED;
 	last_x = curr_x;
 	return (0);
 }
