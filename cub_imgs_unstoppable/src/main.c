e18f9fa5e4c6bee695f260d43163f2a833ed9849
Petya Katsarova <pekatsar@f1r5s3.codam.nl>
2025-09-11 15:47:48 +0200

    norminette 2 files

--- cub_imgs_unstoppable/src/main.c
+++ cub_imgs_unstoppable/src/main.c
@@ -6,7 +6,7 @@
 /*   By: pekatsar <pekatsar@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/08/15 20:01:49 by pekatsar      #+#    #+#                 */
-/*   Updated: 2025/08/30 11:59:33 by pekatsar      ########   odam.nl         */
+/*   Updated: 2025/09/11 15:44:47 by pekatsar      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@ static int	close_window(void *param)
 }
 
 /*
-2: keyPress, 3: keyRelease, 17: DestroyNotify(x btn, window close)
-The third argument is the event mask (which bits to listen for).
-Example:
-1L<<0 = KeyPress
-1L<<1 = KeyRelease
-1L<<6 = MotionNotify (mouse move)
-1L<<21 = FocusIn/FocusOut
-0 = No mask needed (for DestroyNotify, event 17)
+	2: keyPress, 3: keyRelease, 17: DestroyNotify(x btn, window close)
+	The third argument is the event mask (which bits to listen for).
+	Example:
+	1L<<0 = KeyPress
+	1L<<1 = KeyRelease
+	1L<<6 = MotionNotify (mouse move)
+	1L<<21 = FocusIn/FocusOut
+	0 = No mask needed (for DestroyNotify, event 17)
 */
 int	main(int argc, char **argv)
 {
@@ -38,19 +38,19 @@ int	main(int argc, char **argv)
 	t_pl			pl;
 	t_game_configs	*game_configs;
 
-    game_configs = get_input(argc, argv);
-    if (init_data(&data, &pl, game_configs))
-    {
-        free_game_configs(game_configs);
-        return (1);
-    }
-    mlx_loop_hook(data.mlx, render_frame, &data);
-    mlx_hook(data.win, 2, 1L<<0, key_press, &data); // 
-    mlx_hook(data.win, 3, 1L<<1, key_release, &data); 
-    mlx_hook(data.win, 17, 0, close_window, &data);
-    mlx_hook(data.win, 6, 1L<<6, mouse_handler, &data);
-    mlx_hook(data.win, 9, 1L<<21, focus_in, &data);
-    mlx_hook(data.win, 10, 1L<<21, focus_out, &data);
-    mlx_loop(data.mlx);
-    return (0);
+	game_configs = get_input(argc, argv);
+	if (init_data(&data, &pl, game_configs))
+	{
+		free_game_configs(game_configs);
+		return (1);
+	}
+	mlx_loop_hook(data.mlx, render_frame, &data);
+	mlx_hook(data.win, 2, 1L << 0, key_press, &data);
+	mlx_hook(data.win, 3, 1L << 1, key_release, &data);
+	mlx_hook(data.win, 17, 0, close_window, &data);
+	mlx_hook(data.win, 6, 1L << 6, mouse_handler, &data);
+	mlx_hook(data.win, 9, 1L << 21, focus_in, &data);
+	mlx_hook(data.win, 10, 1L << 21, focus_out, &data);
+	mlx_loop(data.mlx);
+	return (0);
 }
--- cub_imgs_unstoppable/src/render/horizontal_check.c
+++ cub_imgs_unstoppable/src/render/horizontal_check.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   horizontal_check.c                                 :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: petya <petya@student.42.fr>                +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/15 19:50:49 by pekatsar          #+#    #+#             */
-/*   Updated: 2025/09/11 07:55:03 by petya            ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   horizontal_check.c                                 :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: petya <petya@student.42.fr>                  +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/08/15 19:50:49 by pekatsar      #+#    #+#                 */
+/*   Updated: 2025/09/11 15:43:39 by pekatsar      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,57 +30,66 @@ void	init_h_ray_up(t_data *d, t_ray_params *ray_params)
 }
 
 /*
-d->pl->y / TILE_SIZE: Get the tile index (row) where the player is.
-floor(...): Get the integer part (the start of the tile).
-* TILE_SIZE: Convert back to pixel position (top edge of the tile).
-+ TILE_SIZE: Move to the bottom edge of the tile (for rays facing down).
-- EPS: Move just inside the tile to avoid floating-point bugs.
+	d->pl->y / TILE_SIZE: Get the tile index (row) where the player is.
+	floor(...): Get the integer part (the start of the tile).
+	* TILE_SIZE: Convert back to pixel position (top edge of the tile).
+	+ TILE_SIZE: Move to the bottom edge of the tile (for rays facing down).
+	- EPS: Move just inside the tile to avoid floating-point bugs.
 */
 void	init_h_ray_down(t_data *d, t_ray_params *params)
 {
 	params->hy = floor(d->pl->y / TILE_SIZE) * TILE_SIZE
 		+ TILE_SIZE + EPS;
-	params->hx = (d->pl->y - params->hy)
-		* params->h_tan + d->pl->x;
+	params->hx = (d->pl->y - params->hy) * params->h_tan + d->pl->x;
 	params->y_offset = TILE_SIZE;
 	params->x_offset = -params->y_offset * params->h_tan;
 }
 
+static bool	is_tiny_intile(t_data *d, t_ray_params *ray_params,
+				int map_x, int map_y)
+{
+	double	x_in_tile;
+	double	y_in_tile;
+
+	x_in_tile = fmod(ray_params->hx, TILE_SIZE);
+	y_in_tile = fmod(ray_params->hy, TILE_SIZE);
+	if (x_in_tile < 0.005 && map_x > 0)
+		if (d->map[map_y][map_x - 1] == '1')
+			return (true);
+	if (x_in_tile > TILE_SIZE - 0.005 && map_x < d->map_cols - 1)
+		if (d->map[map_y][map_x + 1] == '1')
+			return (true);
+	if (y_in_tile < 0.005 && map_y > 0)
+		if (d->map[map_y - 1][map_x] == '1')
+			return (true);
+	if (y_in_tile > TILE_SIZE - 0.005 && map_y < d->map_rows - 1)
+		if (d->map[map_y + 1][map_x] == '1')
+			return (true);
+	return (false);
+}
+
 static void	h_check_helper(t_data *d, t_ray_params *ray_params)
 {
-    int	map_x;
-    int	map_y;
+	int	map_x;
+	int	map_y;
 
-    map_x = 0;
-    map_y = 0;
-    while (ray_params->dof < (d->map_cols + d->map_rows) * 4)
-    {
-        map_x = (int)(ray_params->hx / TILE_SIZE);
-        map_y = (int)(ray_params->hy / TILE_SIZE);
-        if (map_x < 0 || map_x >= d->map_cols
-            || map_y < 0 || map_y >= d->map_rows)
-            break ;
-        if (d->map[map_y][map_x] == '1')
-            break ;
-        double x_in_tile = fmod(ray_params->hx, TILE_SIZE);
-        double y_in_tile = fmod(ray_params->hy, TILE_SIZE);
-        if (x_in_tile < 0.005 && map_x > 0)
-            if (d->map[map_y][map_x - 1] == '1')
-                break;
-        if (x_in_tile > TILE_SIZE - 0.005 && map_x < d->map_cols - 1)
-            if (d->map[map_y][map_x + 1] == '1')
-                break;
-        if (y_in_tile < 0.005 && map_y > 0)
-            if (d->map[map_y - 1][map_x] == '1')
-                break;
-        if (y_in_tile > TILE_SIZE - 0.005 && map_y < d->map_rows - 1)
-            if (d->map[map_y + 1][map_x] == '1')
-                break;
-        
-        ray_params->hx += ray_params->x_offset;
-        ray_params->hy += ray_params->y_offset;
-        ray_params->dof++;
-    }
+	map_x = 0;
+	map_y = 0;
+	while (ray_params->dof < (d->map_cols + d->map_rows) * 4)
+	{
+		map_x = (int)(ray_params->hx / TILE_SIZE);
+		map_y = (int)(ray_params->hy / TILE_SIZE);
+		if (map_x < 0 || map_x >= d->map_cols
+			|| map_y < 0 || map_y >= d->map_rows)
+			break ;
+		if (d->map[map_y][map_x] == '1')
+			break ;
+		if (is_tiny_intile(d, ray_params, map_x, map_y))
+			break ;
+		ray_params->hx += ray_params->x_offset;
+		ray_params->hy += ray_params->y_offset;
+		ray_params->dof++;
+	}
 }
 
 void	horizontal_check(t_ray *ray, t_data *d, t_ray_params *ray_params)
